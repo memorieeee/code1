2741d4e4e0da69118ca3fc9aa96db50212c565fc
minjeong <leemj29299@gmail.com>
2022-10-20 17:59:22 +0900

    .

--- children/js/main.js
+++ children/js/main.js
@@ -15,13 +15,18 @@ $(document).ready(function(){
 		},
 
 		onLeave: function(origin, destination, direction, trigger){
-			console.log(destination);
+			//console.log(destination);
 			if((destination.index == 2)||(destination.index == 3)||(destination.index == 4)||(destination.index == 5)){
 				$('.header').addClass('black');
 				$('#fp-nav').addClass('black');
 			}else{
 				$('.header').removeClass('black');
 				$('#fp-nav').removeClass('black');
+			}
+		},
+		afterLoad: function(origin, destination, direction, trigger){
+			if(destination.index == 1){ //보건통계 슬라이드가 나타났을때
+				funCount();
 			}
 		},
 
@@ -34,11 +39,38 @@ $(document).ready(function(){
 
 	});//fullpage
 
-	//count 숫자 count-up
-	// $('.count .rate li .num strong').counterUp({   /* 상세설정을 해주는 경우 */
-	// delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
-	// time: 2000    /* 전체 애니메이션 시간 */
-	// });//count-up
+	/* 보건통계 숫자 카운팅 효과 */ 
 
+	function funCount(){
+		console.log('카운팅동작');
+		$('.count .rate li .num strong').counterUp({
+			time: 1000    /* 전체 애니메이션 시간 */
+		});
+	}
+
+	/* 최근 소식에 swiper 효과 */
+	const swiper = new Swiper('.news .list', { /* 팝업을 감싸는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			480: {    /* 480px 이상일때 적용 */
+			slidesPerView: 2,
+			spaceBetween: 20,
+			},
+			700: {    /* 800px 이상일때 적용 */
+			slidesPerView: 3,
+			spaceBetween: 30,
+			},
+			1000: {    /* 1200px 이상일때 적용 */
+			slidesPerView: 4,
+			spaceBetween: 40,
+			},
+		},
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.btn_paging', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		},
+
+});
 	
 });//document.ready
