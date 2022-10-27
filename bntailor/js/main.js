774968b1d606e01840c2e33fc2033ea9231d1032
minjeong <leemj29299@gmail.com>
2022-10-27 12:52:53 +0900

    .

--- bntailor/js/main.js
+++ bntailor/js/main.js
@@ -16,5 +16,24 @@ $(document).ready(function(){
 	},
 
 
-});
+	});//visual swiper
+
+	/* visual popup의 정지/재생 버튼 
+		하나의 버튼에 두가지 기능
+		정지/재생 기능 구분하는 값 
+		btn_stop버튼에 play클래스가 없으면 일시정지
+		play클래스가 있으면 재생 
+	*/ 
+	$('.visual .popup .btn_stop').on('click', function(){
+		let popStatus = $(this).hasClass('play'); //true면 play - 재생기능. false면 stop - 정지기능
+		if(popStatus == true){ //버튼 상태가 play모양 - 현재 정지상태 - 재생기능을 실행
+			swiper.autoplay.start();
+			$(this).removeClass('play');
+			$(this).text('일시정지');
+		}else{ //버튼의 상태가 일시정지 모양 - 현재 재생상태 - 일시정지 기능 실행 
+			swiper.autoplay.stop(); 
+			$(this).addClass('play');
+			$(this).text('재생');
+		}
+	});
 });
