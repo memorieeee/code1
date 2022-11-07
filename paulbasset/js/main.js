826b2f046b193af4834cfd58e3e2fb1f6b3b6223
minjeong <leemj29299@gmail.com>
2022-11-07 18:00:34 +0900

    폴바셋 코딩

--- paulbasset/js/main.js
+++ paulbasset/js/main.js
@@ -29,4 +29,36 @@ $(document).ready(function(){
 		}
 	}); // visual stop
 
+    const swiper_menu = new Swiper('.menu .list', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 20, /* li와 li사이 - 제일 작은 여백 */
+        
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+                spaceBetween: 38, 
+            },
+        
+        },
+    });
+
+    const swiper_news = new Swiper('.news .list', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.news .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+    	},
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+        },
+        navigation: {  /* 이전, 다음 버튼 */
+		nextEl: '.news .btn .right',  /* 다음 버튼의 클래스명 */
+		prevEl: '.news .btn .left',  
+    	},
+
+    });
+
 });//document.ready
